0119e5a8b09d15aa59d140a3d2088524e547680a
Andrey Egorov <andreyegorov1985@gmail.com>
2021-05-09 00:05:26 +0300

    feat: add exercise 3 and 4

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,25 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    if (self.count == 0) {
+        return nil;
+    }
+    long left = 0;
+    long right = self.count - 1;
+    long middle;
+    while (left < right) {
+        middle = left + (right - left) / 2;
+        if ([[self objectAtIndex:left] doubleValue] < [[self objectAtIndex:right] doubleValue]) {
+            return [self objectAtIndex:left];
+        }
+        if ([[self objectAtIndex:middle] doubleValue] >= [[self objectAtIndex:left] doubleValue]) {
+            left = middle + 1;
+        }
+        else {
+            right = middle;
+        }
+    }
+    return [self objectAtIndex:left];
 }
 
 @end
--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,29 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    if (number == nil) {
+        return @[];
+    }
+    
+    NSString *num_str = [[NSString alloc] initWithFormat: @"%@", number];
+
+    NSMutableString *reversedString = [NSMutableString new];
+    while ([num_str length] != [reversedString length]) {
+        NSRange range = NSMakeRange([num_str length] - [reversedString length] - 1, 1);
+        [reversedString appendString: [num_str substringWithRange:range]];
+    }
+    
+    NSMutableArray *result = [[NSMutableArray alloc] initWithCapacity:[reversedString length]];
+    for (int i = 0; i < [reversedString length]; i++) {
+        NSString *strChar  = [NSString stringWithFormat:@"%c", [reversedString characterAtIndex:i]];
+        [result addObject:strChar];
+    }
+    
+    if (number.doubleValue < 0) {
+        [result removeLastObject];
+    }
+    
+    return result;
 }
 
 @end
